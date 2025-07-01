2ccc01e7dfb66c61fae61baeb2435fcc29110a6f
Jiwoo Seo <sjwoos123@gmail.com>
2025-07-01 14:55:46 +0900

    step 2

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,15 +4,12 @@ import { useEffect, useState } from "react";
 
 export default function Page() {
 
-    const [posts, setPosts] = useState<{id: number, title: string}[]>([]);
+    const [posts, setPosts] = useState<{ id: number, title: string }[]>([]);
 
     useEffect(() => {
-        setPosts([
-            {id: 1, title: "명언 1"},
-            {id: 2, title: "명언 2"},
-            {id: 3, title: "명언 3"},
-            {id: 4, title: "명언 4"},
-        ]);
+        fetch("http://localhost:8080/api/v1/posts")
+        .then((res) => res.json())
+        .then(setPosts);
     }, []);
 
   return (
@@ -20,6 +17,7 @@ export default function Page() {
       <h1>글 목록</h1>
 
       <ul>
+        {posts.length === 0 && <li>로딩중...</li>}
         {posts.map((post) => (
             <li key={post.id}>
                 {post.title}
