419ab942378b50d8520ae665daf2850c760b0365
Jiwoo Seo <sjwoos123@gmail.com>
2025-07-02 11:36:07 +0900

    work

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -9,27 +9,31 @@ const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export default function Page() {
 
-    const [posts, setPosts] = useState<PostDto[]>([]);
+    const [posts, setPosts] = useState<PostDto[] | null>(null);
 
     useEffect(() => {
       apiFetch(`/api/v1/posts`).then(setPosts);
     }, []);
 
+    if (posts == null) return <div>로딩중...</div>;
+
   return (
     <>
       <h1>글 목록</h1>
 
-      {posts.length === 0 && <div>로딩중...</div>}
+      {posts.length == 0 && <div>글이 없습니다.</div>}
 
-      <ul>
-      {posts.map((post) => (
-          <li key={post.id}>
-            <Link href={`/posts/${post.id}`}>
-              {post.id} : {post.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {posts.length > 0 && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <Link href={`/posts/${post.id}`}>
+                {post.id} : {post.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
       <div>
         <Link href="/posts/write">글쓰기</Link>
       </div>
