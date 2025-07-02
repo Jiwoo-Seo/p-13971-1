2d405c051d6c9599f123159eeb1c59cba62866ac
Jiwoo Seo <sjwoos123@gmail.com>
2025-07-02 14:15:18 +0900

    work

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,19 +1,13 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { apiFetch } from "@/lib/backend/clients";
+import type { PostCommentDto, PostWithContentDto } from "@/type/post";
 import Link from "next/link";
-import type { PostWithContentDto, PostCommentDto } from "@/type/post";
+import { useRouter } from "next/navigation";
 import { use, useEffect, useState } from "react";
 
-export default function Page({ params }: { params: Promise<{ id: number }> }) {
-  const { id } = use(params);
+function PostInfo({ post }: { post: PostWithContentDto }) {
   const router = useRouter();
-
-  const [post, setPost] = useState< PostWithContentDto | null>(null);
-  const [postComments, setPostComments] = useState<PostCommentDto[] | null>(
-    null
-  );
 
   const deletePost = (id: number) => {
     apiFetch(`/api/v1/posts/${id}`, {
@@ -24,14 +18,48 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
     });
   };
 
+  return (
+    <>
+      <div>번호 : {post.id}</div>
+      <div>제목: {post.title}</div>
+      <div style={{ whiteSpace: "pre-line" }}>{post.content}</div>
+
+      <div className="flex gap-2">
+        <button
+          className="p-2 rounded border"
+          onClick={() =>
+            confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`) &&
+            deletePost(post.id)
+          }
+        >
+          삭제
+        </button>
+        <Link className="p-2 rounded border" href={`/posts/${post.id}/edit`}>
+          수정
+        </Link>
+      </div>
+    </>
+  );
+}
+
+function PostCommentWriteAndList({
+  id,
+  postComments,
+  setPostComments,
+}: {
+  id: number;
+  postComments: PostCommentDto[] | null;
+  setPostComments: (postComments: PostCommentDto[]) => void;
+}) {
   const deleteComment = (id: number, commentId: number) => {
     apiFetch(`/api/v1/posts/${id}/comments/${commentId}`, {
       method: "DELETE",
     }).then((data) => {
       alert(data.msg);
-      if (postComments != null) {
-        setPostComments(postComments.filter((c) => c.id != commentId));
-      }
+
+      if (postComments == null) return;
+
+      setPostComments(postComments.filter((c) => c.id != commentId));
     });
   };
 
@@ -75,80 +103,88 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
     });
   };
 
+  return (
+    <>
+      <h2>댓글 작성</h2>
+
+      <form className="p-2" onSubmit={handleCommentWriteFormSubmit}>
+        <textarea
+          className="border p-2 rounded"
+          name="content"
+          placeholder="댓글 내용"
+          maxLength={100}
+          rows={5}
+        />
+        <button className="p-2 rounded border" type="submit">
+          작성
+        </button>
+      </form>
+
+      <h2>댓글 목록</h2>
+
+      {postComments == null && <div>댓글 로딩중...</div>}
+
+      {postComments != null && postComments.length == 0 && (
+        <div>댓글이 없습니다.</div>
+      )}
+
+      {postComments != null && postComments.length > 0 && (
+        <ul>
+          {postComments.map((comment) => (
+            <li key={comment.id}>
+              {comment.id} : {comment.content}
+              <button
+                className="p-2 rounded border"
+                onClick={() =>
+                  confirm(`${comment.id}번 댓글을 정말로 삭제하시겠습니까?`) &&
+                  deleteComment(id, comment.id)
+                }
+              >
+                삭제
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default function Page({ params }: { params: Promise<{ id: number }> }) {
+  const { id } = use(params);
+
+  const [post, setPost] = useState<PostWithContentDto | null>(null);
+  const [postComments, setPostComments] = useState<PostCommentDto[] | null>(
+    null
+  );
 
   useEffect(() => {
     apiFetch(`/api/v1/posts/${id}`)
       .then(setPost)
       .catch((error) => {
-        alert(error.message);
+        alert(`${error.resultCode} : ${error.msg}`);
       });
 
     apiFetch(`/api/v1/posts/${id}/comments`)
       .then(setPostComments)
       .catch((error) => {
-        alert(error.message);
+        alert(`${error.resultCode} : ${error.msg}`);
       });
   }, []);
 
   if (post == null) return <div>로딩중...</div>;
 
-
   return (
     <>
       <h1>글 상세페이지</h1>
 
-        <div>번호 : {post.id}</div>
-        <div>제목: {post.title}</div>
-        <div style={{ whiteSpace: "pre-line" }}>{post.content}</div>
-        <div>
-          <button className="p-2 rounded border"
-           onClick={() => 
-            confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`) &&
-            deletePost(post.id)}>삭제</button>
-          <Link className="p-2 rounded border"
-          href={`/posts/${post.id}/edit`}>수정</Link>
-        </div>
+      <PostInfo post={post} />
 
-        <h2>댓글 작성</h2>
-
-        <form className="p-2" onSubmit={handleCommentWriteFormSubmit}>
-          <textarea className="border p-2 rounded"
-           name="content" 
-           placeholder="댓글 내용"
-           maxLength={100}
-           rows={5}
-           />
-          <button className="p-2 rounded border" type="submit">
-            저장
-          </button>
-        </form>
-        
-        <h2>댓글 목록</h2>
-
-        {postComments == null && <div>댓글 로딩중...</div>}
-
-        {postComments != null && postComments.length == 0 && (
-          <div>댓글이 없습니다.</div>
-        )}
-
-        {postComments != null && postComments.length > 0 && (
-          <ul>
-            {postComments.map((comment) => (
-              <li key={comment.id}>
-                {comment.id} : {comment.content}
-                <button
-                  className="p-2 rounded border"
-                  onClick={() =>
-                    confirm(`${comment.id}번 댓글을 정말로 삭제하시겠습니까?`) &&
-                    deleteComment(id, comment.id)
-                  }
-                >
-                  삭제
-                </button>
-              </li>
-            ))}
-          </ul>
-        )}
+      <PostCommentWriteAndList
+        id={id}
+        postComments={postComments}
+        setPostComments={setPostComments}
+      />
     </>
   );
 }
