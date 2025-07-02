d5ae5b6a16af5ad22fce70871d2891bc8f5887dc
Jiwoo Seo <sjwoos123@gmail.com>
2025-07-02 09:52:45 +0900

    step1

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 
+import { apiFetch } from "@/lib/backend/clients";
 import type { PostDto } from "@/type/post";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+
+const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export default function Page() {
 
     const [posts, setPosts] = useState<PostDto[]>([]);
 
     useEffect(() => {
-        fetch("http://localhost:8080/api/v1/posts")
-        .then((res) => res.json())
-        .then(setPosts);
+      apiFetch(`/api/v1/posts`).then(setPosts);
     }, []);
 
   return (
