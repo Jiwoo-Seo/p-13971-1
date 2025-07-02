81a3ae1404169fa914a85db47cf15b8b91b07b08
Jiwoo Seo <sjwoos123@gmail.com>
2025-07-02 12:13:06 +0900

    step5

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -62,6 +62,10 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
     }).then((data) => {
       alert(data.msg);
       contentTextarea.value = "";
+
+      if (postComments == null) return;
+
+      setPostComments([...postComments, data.data]);
     });
   };
 
@@ -114,7 +118,7 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
           <ul>
             {postComments.map((comment) => (
               <li key={comment.id}>
-                {comment.content}
+                {comment.id} : {comment.content}
                 <button
                   className="p-2 rounded border"
                   onClick={() =>
